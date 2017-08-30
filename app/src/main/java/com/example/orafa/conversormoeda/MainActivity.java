7b18b1ae0eb7f2e837e1789831650b05a6407517
o Rafa <paracafa@gmail.com>
2017-08-30 18:39:07 -0300

    Aula 14 - Adicionado @+id e recebendo findViewById no MainActivity com classe criiada ViewHolder estanciada

--- app/src/main/java/com/example/orafa/conversormoeda/MainActivity.java
+++ app/src/main/java/com/example/orafa/conversormoeda/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.orafa.conversormoeda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder nViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.nViewHolder.editValue = (EditText)findViewById(R.id.edit_value);
+        this.nViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
+        this.nViewHolder.textEuro = (TextView)findViewById(R.id.text_euro);
+        this.nViewHolder.buttomCalculate = (Button) findViewById(R.id.button_calculate);
     }
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttomCalculate;
+    }
+
 }
