ca161cfec577c3ed18fc10bafe3702ee19d7f400
o Rafa <paracafa@gmail.com>
2017-08-31 17:35:37 -0300

    Colocando OnClick no button, três métodos.

--- app/src/main/java/com/example/orafa/conversormoeda/MainActivity.java
+++ app/src/main/java/com/example/orafa/conversormoeda/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.orafa.conversormoeda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+//terceiro método para onclick>implements View.OnClickListener
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder nViewHolder = new ViewHolder();
 
@@ -19,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
         this.nViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
         this.nViewHolder.textEuro = (TextView)findViewById(R.id.text_euro);
         this.nViewHolder.buttomCalculate = (Button) findViewById(R.id.button_calculate);
+
+        //segundo método para onclick
+        this.nViewHolder.buttomCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //lógica para o método dois
+            }
+        });
+
+        //terceiro método
+        this.nViewHolder.buttomCalculate.setOnClickListener(this);
+    }
+
+    public void teste(View view) {
+        //lógica para o botão. Prrimeiro método
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.button_calculate){
+            //lógic para o botão. terceiro métodp
+        }
     }
 
     private static class ViewHolder{
@@ -27,5 +53,7 @@ public class MainActivity extends AppCompatActivity {
         TextView textEuro;
         Button buttomCalculate;
     }
+
+
 
 }
